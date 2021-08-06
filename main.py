117d7743cc3d26556da58c896785d99a825735ce
TheMiner_02 <87092444+TheMiner02@users.noreply.github.com>
2021-08-06 19:49:49 +0200

    Added files

---
+++ main.py
@@ -0,0 +1,125 @@
+import discord
+import os
+import json
+import requests
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+# ---
+
+def showHelp():
+    commands = '__The commands:__\n\n**$help** - Shows this message.\n**$stats <channel>** - Provides stats for the specified YouTube channel. Use the channel ID. The usage of channel names is still in developement.'
+    return (commands)
+
+# ---
+
+def checkAccount(channel_id):
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key="
+        + os.getenv('GOOGLEAPI'))
+    data = json.loads(response.text)
+    return (data['pageInfo']['totalResults'])
+
+# Data for $stats
+def stats_getSubs(channel_id):
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key="
+        + os.getenv('GOOGLEAPI'))
+    data = json.loads(response.text)
+    return (data['items'][0]['statistics']['subscriberCount'])
+
+
+def stats_getVideos(channel_id):
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key="
+        + os.getenv('GOOGLEAPI'))
+    data = json.loads(response.text)
+    return (data['items'][0]['statistics']['videoCount'])
+
+
+def stats_getViews(channel_id):
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key="
+        + os.getenv('GOOGLEAPI'))
+    data = json.loads(response.text)
+    return (data['items'][0]['statistics']['viewCount'])
+
+# ---
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+# ---------------------------------------------------------------
+# $help - Shows the list of commands.
+# ---------------------------------------------------------------
+
+    if message.content.startswith('$help'):
+        help = showHelp()
+        await message.channel.send(help)
+        print(message.guild.name + ' / ' + message.channel.name + ' - Help sent')
+        return
+
+# ---------------------------------------------------------------
+# $stats - Provides the stats.
+# ---------------------------------------------------------------
+
+    if message.content.startswith('$stats'):
+
+        unknown_channel = "`I don't know this channel. Please try another one.`"
+        try:
+          channel = message.content.split("$stats ", 1)[1]
+        except:
+          await message.channel.send(unknown_channel)
+          print(message.guild.name + ' / ' + message.channel.name + ' - Empty channel')
+          return
+        yt_results = checkAccount(channel)
+
+        # Response:
+        if yt_results == 0:
+          await message.channel.send(unknown_channel)
+          print(message.guild.name + ' / ' + message.channel.name + ' - Unknown channel - Channel: ' + channel)
+          return
+        else:
+          yt_subs = stats_getSubs(channel)
+          yt_videos = stats_getVideos(channel)
+          yt_views = stats_getViews(channel)
+          # ---
+          statsMessage = discord.Embed(title="YouTube Stats", description="**Channel: **" + channel, color=0xf50000)
+          statsMessage.add_field(name="Subscribers", value=yt_subs, inline=False)
+          statsMessage.add_field(name="Total videos", value=yt_videos, inline=False)
+          statsMessage.add_field(name="Total views", value=yt_views, inline=False)
+          # ---
+          await message.channel.send(embed=statsMessage)
+          print(message.guild.name + ' / ' + message.channel.name + ' - Stats sent - Channel: ' + channel)
+          return
+
+# ---------------------------------------------------------------
+# $ - Unknown commands
+# ---------------------------------------------------------------
+
+    if message.content.startswith('$'):
+        await message.channel.send('```I dont know this command. Try $help```')
+        print('Unknown Command - Answer sent')
+        return
+
+
+# ---------------------------------------------------------------
+# Add reaction to messages that contain TubeStat
+# ---------------------------------------------------------------
+
+    if 'TubeStat' in message.content:
+        reaction = '👀'
+        await message.add_reaction(reaction)
+        print(message.guild.name + ' / ' + message.channel.name + 'Added a reaction')
+        return
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
