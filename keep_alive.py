ce585162d9c066b180868c1713d822ce6ddb9b80
TheMiner_02 <87092444+TheMiner02@users.noreply.github.com>
2021-08-17 10:01:03 +0200

    Added displayServers()

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from threading import Thread
+# from main import displayServers
 
 app = Flask('')
 
 @app.route('/')
 def home():
-  return "Bot is alive."
+  return "Bot is alive.\n" # + displayServers
 
 def run():
   app.run(host='0.0.0.0',port=8080)
